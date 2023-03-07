7eecb5c5c5b8b323a40ae25bbf1f52e86a884ece
haileye4 <hailey.e.johnson3@gmail.com>
2023-03-06 22:06:35 -0700

    Worked on scores JS

--- scores.js
+++ scores.js
@@ -1,36 +1,36 @@
-function loadScores() {
-    let scores = [];
-    const scoresText = localStorage.getItem('scores');
-    if (scoresText) {
-      scores = JSON.parse(scoresText);
+function loadResults() {
+    let results = [];
+    const resultsText = localStorage.getItem('allResults');
+    if (resultsText) {
+      results = JSON.parse(resultsText);
     }
   
-    const tableBodyEl = document.querySelector('#scores');
+    const tableBodyEl = document.querySelector('#results');
   
-    if (scores.length) {
-      for (const [i, score] of scores.entries()) {
+    if (results.length) {
+      for (const [i, result] of results.entries()) {
         const positionTdEl = document.createElement('td');
         const nameTdEl = document.createElement('td');
-        const scoreTdEl = document.createElement('td');
+        const resultTdEl = document.createElement('td');
         const dateTdEl = document.createElement('td');
   
         positionTdEl.textContent = i + 1;
-        nameTdEl.textContent = score.name;
-        scoreTdEl.textContent = score.score;
-        dateTdEl.textContent = score.date;
+        nameTdEl.textContent = result.name;
+        resultTdEl.textContent = result.ending;
+        dateTdEl.textContent = result.date;
   
         const rowEl = document.createElement('tr');
         rowEl.appendChild(positionTdEl);
         rowEl.appendChild(nameTdEl);
-        rowEl.appendChild(scoreTdEl);
+        rowEl.appendChild(resultTdEl);
         rowEl.appendChild(dateTdEl);
   
         tableBodyEl.appendChild(rowEl);
       }
     } else {
-      tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to score</td></tr>';
+      tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first!</td></tr>';
     }
   }
   
-  loadScores();
+  loadResults();
   
